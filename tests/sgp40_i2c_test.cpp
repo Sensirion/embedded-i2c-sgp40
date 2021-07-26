bce91909f24b58bfc9ac1c66f4d5cd701115fc34
Bjoern Muntwyler <Bjoern.Muntwyler@sensirion.com>
2021-07-26 16:20:28 +0200

    Adjust tests

--- tests/sgp40_i2c_test.cpp
+++ tests/sgp40_i2c_test.cpp
@@ -45,17 +45,12 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-// TODO: DRIVER_GENERATOR Remove commands which shouldn't be tested
-// TODO: DRIVER_GENERATOR Adjust setup and teardown
-// TODO: DRIVER_GENERATOR Adjust all tests such that pre- and post conditions
-// are meet
-
 TEST_GROUP (SGP40_Tests) {
     void setup() {
         sensirion_i2c_hal_init();
 
-        // Select MUX 1 channel 1 (TODO: DRIVER_GENERATOR choose correct mux
-        // position)
+        // Select MUX 1 channel 1 (No SGP40 attached yet... choose correct mux
+        // position when SGP40 added to testbed)
         int16_t error = sensirion_i2c_mux_set_single_channel(0x71, 1);
         CHECK_EQUAL_ZERO_TEXT(error, "sensirion_i2c_mux_set_single_channel")
     }
