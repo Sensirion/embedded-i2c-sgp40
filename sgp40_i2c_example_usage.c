0c93b9a728a8e426ef663651d28b435988d88467
lfierz <leonie.fierz@sensirion.com>
2021-09-22 10:09:26 +0200

    LKD-410 clean up of sensor interface
    update to cleaned up command set
    do measurement loop for 1 minute
    getting serial was added on interface and example

--- sgp40_i2c_example_usage.c
+++ sgp40_i2c_example_usage.c
@@ -1,7 +1,7 @@
 /*
- * I2C-Generator: 0.2.0
+ * I2C-Generator: 0.3.0
  * Yaml Version: 0.1.0
- * Template Version: 0.7.0-24-g00f1ef2
+ * Template Version: 0.7.0-62-g3d691f9
  */
 /*
  * Copyright (c) 2021, Sensirion AG
@@ -39,6 +39,7 @@
 #include "sensirion_common.h"
 #include "sensirion_i2c_hal.h"
 #include "sgp40_i2c.h"
+// #include <inttypes.h>
 
 /*
  * TO USE CONSOLE OUTPUT (PRINTF) YOU MAY NEED TO ADAPT THE INCLUDE ABOVE OR
@@ -47,7 +48,26 @@
  */
 
 int main(void) {
+    int16_t error = 0;
+
     sensirion_i2c_hal_init();
+
+    uint16_t serial_number[3];
+    uint8_t serial_number_size = 3;
+
+    error = sgp40_get_serial_number(serial_number, serial_number_size);
+
+    if (error) {
+        printf("Error executing sgp40_get_serial_number(): %i\n", error);
+    } else {
+        // printf("Serial number: %" PRIu64 "\n",
+        //         (((uint64_t)serial_number[0]) << 32) |
+        //         (((uint64_t)serial_number[1]) << 16) |
+        //         ((uint64_t)serial_number[2]));
+        printf("serial: 0x%04x%04x%04x\n", serial_number[0], serial_number[1],
+               serial_number[2]);
+        printf("\n");
+    }
 
     // Start Measurement
 
@@ -55,15 +75,14 @@ int main(void) {
     uint16_t default_rh = 0x8000;
     uint16_t default_t = 0x6666;
 
-    for (;;) {
-        int16_t error = 0;
+    for (int i = 0; i < 60; i++) {
         uint16_t sraw_voc;
 
         sensirion_i2c_hal_sleep_usec(1000000);
 
-        error = sgp40_measure_raw(default_rh, default_t, &sraw_voc);
+        error = sgp40_measure_raw_signal(default_rh, default_t, &sraw_voc);
         if (error) {
-            printf("Error executing sgp40_measure_raw(): "
+            printf("Error executing sgp40_measure_raw_signal(): "
                    "%i\n",
                    error);
         } else {
